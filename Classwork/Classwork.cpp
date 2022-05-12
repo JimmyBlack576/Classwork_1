e4f1ee5d55eda4589bae096050f155c81e82df66
Jimmy Blackwood <Jimmy Blackwood@DESKTOP-AOM0ABL>
2022-05-12 13:10:36 +0300

    Добавьте файлы проекта.

---
+++ Classwork/Classwork.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+
+    int a;
+    cout << "Введите число: ";
+    cin >> a;
+    
+    (a % 2) ? cout << "Нечётное " : cout << "Чётное " << endl;
+}
+
